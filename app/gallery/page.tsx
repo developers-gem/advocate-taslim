ddad6f7614964c61137ff1c35f8a3532b2d6e0d4
Pragati Singh <pragatisingh0417@gmail.com>
2026-06-09 18:49:02 +0530

    uopdated code

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,14 +1,13 @@
 import Image from "next/image";
 
 const galleryImages = [
-  "/images/gallery/gallery-1.jpg",
-  "/images/gallery/gallery-2.jpg",
-  "/images/gallery/gallery-3.jpg",
-  "/images/gallery/gallery-4.jpg",
-  "/images/gallery/gallery-5.jpg",
-  "/images/gallery/gallery-6.jpg",
-  "/images/gallery/gallery-7.jpg",
-  "/images/gallery/gallery-8.jpg",
+  "/images/court-2.jpg",
+  "/images/about-img.jpg",
+  "/images/banner-1.jpg",
+  "/images/court.jpg",
+  "/images/service-4.jpg",
+  "/images/Marriage-1.jpg",
+  
 ];
 
 export default function GalleryPage() {
